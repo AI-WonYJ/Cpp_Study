00c1a1b4e040dc603398334932e0c29b264ce994
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-11-06 23:26:30 +0900

    Add files via upload

---
+++ c++/class1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//   int hour, min;
+//   cin >> hour;
+//   cin >> min;
+//   cout << "The time is " << hour << ":" << min << endl;
+
+//   return 0;
+// }
+
+
+
+// int main() {
+//   int score;
+//   cin >> score;
+//   if (score >= 95) {
+//     cout << "A+";
+//   }
+//   else if (score >= 90) {
+//     cout << "A0";
+//   }
+//   else if (score >= 85) {
+//     cout << "B+";
+//   }
+//   else if (score >= 80) {
+//     cout << "B0";
+//   }
+//   else if (score >= 75) {
+//     cout << "C+";
+//   }
+//   else if (score >= 70) {
+//     cout << "C";
+//   }
+//   else if (score >= 65) {
+//     cout << "D+";
+//   }
+//   else if (score >= 60) {
+//     cout << "D0";
+//   }
+//   else {
+//     cout << "F";
+//   }
+// }
+
+
+
+int main() {
+	for (/* 초기식 */; /* 조건식 */; /* 증감식 */) {
+		/* 소스코드 */
+	}
+	
+	return 0;
+}
